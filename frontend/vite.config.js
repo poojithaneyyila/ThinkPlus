f643d11c7fb3217cb711392230bfd5745a3a036f
NEYYILA POOJITHA <poojithaneyyila@gmail.com>
2026-02-20 10:25:00 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: path.resolve(__dirname),  // frontend folder is root
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"), // explicitly use frontend/index.html
+    },
+  },
+});
